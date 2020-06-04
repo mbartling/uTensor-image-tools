4e041eb1cff6d099fed8e52fcf193fb1ffa998a0
Michael Bartling <michael.bartling15@gmail.com>
2020-06-04 10:28:06 -0500

    Add bitmap loader

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
-#include "uTensor.h"
-
 #include <iostream>
+#include "uTensor.h"
+#include "bitmap_image.hpp"
 
-int main() {
+using std::cout;
+using std::endl;
 
+int main (int argc, char *argv[]) {
+  if (argc != 2) {
+    cout << "Unexpected number of command line args" << endl << "Please provide a target bmp file to load" << endl;
+  }
   return 0;
 }
