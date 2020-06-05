fe41492f47ea669887aa1b973aa3243fe5bb1874
Michael Bartling <michael.bartling15@gmail.com>
2020-06-05 10:16:29 -0500

    Turn into library

---
+++ image_tools/SlicedImage.hpp
@@ -0,0 +1,57 @@
+#ifndef SLICED_IMAGE_HPP
+#define SLICED_IMAGE_HPP
+#include "uTensor.h"
+
+template<typename T>
+class SlicedImage {
+  public:
+    SlicedImage(const uTensor::Tensor& t, unsigned int num_row_slices,
+        unsigned int num_col_slices);
+
+    void set_current_slice(uint8_t slice_row, uint8_t slice_col);
+    //const T operator()(uint16_t y, uint16_t x, uint16_t c) const;
+    // Readonly
+    T operator()(uint16_t y, uint16_t x, uint16_t c);
+
+  private:
+    const uTensor::Tensor& image;
+    uint8_t cur_slice_r;
+    uint8_t cur_slice_c;
+
+  public:
+    const unsigned int num_row_slices;
+    const unsigned int num_col_slices;
+    const uint16_t num_slices;
+    const uint16_t sliced_height;
+    const uint16_t sliced_width;
+};
+
+template<typename T>
+SlicedImage<T>::SlicedImage(const uTensor::Tensor& t, unsigned int num_row_slices,
+    unsigned int num_col_slices)
+  : image(t), cur_slice_r(0), cur_slice_c(0),
+  num_row_slices(num_row_slices),
+  num_col_slices(num_col_slices),
+  num_slices(static_cast<uint16_t>(num_row_slices * num_col_slices)),
+  sliced_height(static_cast<uint16_t>(t->get_shape()[1] / num_row_slices)),
+  sliced_width(static_cast<uint16_t>(t->get_shape()[2] / num_col_slices)) {
+    // TODO
+    // Do asserts to make sure there is no remainder from slicing h/w      
+    // Do assert to make sure num_channels == 3
+  }
+
+template<typename T>
+void SlicedImage<T>::set_current_slice(uint8_t slice_row, uint8_t slice_col) { 
+  cur_slice_r = slice_row; 
+  cur_slice_c = slice_col;
+}
+//const T operator()(uint16_t y, uint16_t x, uint16_t c) const {
+//  return static_cast<T>(image(0, y + num_row_slices*sliced_height, x + num_col_slices*sliced_width, c));
+//}
+// Readonly
+template<typename T>
+T SlicedImage<T>::operator()(uint16_t y, uint16_t x, uint16_t c){
+  return static_cast<T>(image(0, y + cur_slice_r*sliced_height, x + cur_slice_c*sliced_width, c));
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -4,141 +4,14 @@
 
 #include "bitmap_image.hpp"
 #include "uTensor.h"
+#include "SlicedImage.hpp"
+#include "Interpolation.hpp"
 
 using std::cout;
 using std::endl;
 using namespace uTensor;
 
 int str2int(const char* str);
-
-class SlicedImage {
-  public:
-    SlicedImage(const Tensor& t, unsigned int num_row_slices,
-                      unsigned int num_col_slices)
-        : image(t), cur_slice_r(0), cur_slice_c(0),
-          num_row_slices(num_row_slices),
-          num_col_slices(num_col_slices),
-          num_slices(static_cast<uint16_t>(num_row_slices * num_col_slices)),
-          sliced_height(static_cast<uint16_t>(t->get_shape()[1] / num_row_slices)),
-          sliced_width(static_cast<uint16_t>(t->get_shape()[2] / num_col_slices)) {
-      // TODO
-      // Do asserts to make sure there is no remainder from slicing h/w      
-      // Do assert to make sure num_channels == 3
-#ifdef CPP_PRINT
-      cout << "Num Slices:    " << num_slices << endl;
-      cout << "Sliced Height: " << sliced_height << endl;
-      cout << "Sliced Width:  " << sliced_width << endl;
-#endif
-    }
-
-    void set_current_slice(uint8_t slice_row, uint8_t slice_col) { 
-      cur_slice_r = slice_row; 
-      cur_slice_c = slice_col;
-    }
-  //const uint8_t operator()(uint16_t y, uint16_t x, uint16_t c) const {
-  //  return static_cast<uint8_t>(image(0, y + num_row_slices*sliced_height, x + num_col_slices*sliced_width, c));
-  //}
-  // Readonly
-  uint8_t operator()(uint16_t y, uint16_t x, uint16_t c){
-    return static_cast<uint8_t>(image(0, y + cur_slice_r*sliced_height, x + cur_slice_c*sliced_width, c));
-  }
-
- private:
-  const Tensor& image;
-  uint8_t cur_slice_r;
-  uint8_t cur_slice_c;
-
- public:
-  const unsigned int num_row_slices;
-  const unsigned int num_col_slices;
-  const uint16_t num_slices;
-  const uint16_t sliced_height;
-  const uint16_t sliced_width;
-};
-
-TensorInterface* bilinear_interpolate(SlicedImage& slicedImage, uint16_t target_size, bool pad_to_square=false) {
-  const uint16_t orig_height = slicedImage.sliced_height;
-  const uint16_t orig_width = slicedImage.sliced_width;
-  bool width_larger = (orig_height < orig_width) ? true : false;
-  uint16_t pad_left = 0;
-  uint16_t pad_top = 0;
-  uint16_t target_height = 0;
-  uint16_t target_width = 0;
-  if(width_larger){
-    target_width = target_size;
-    target_height = static_cast<uint16_t>(static_cast<float>(orig_height)*
-        ( static_cast<float>(target_size) / static_cast<float>(orig_width) ));
-    if(pad_to_square){
-      pad_top = (target_width - target_height) >> 1; // Divide by 2
-      // TODO assert target_height no greater than target_size
-      target_height = target_size;
-    }
-  } else {
-    target_height = target_size;
-    target_width = static_cast<uint16_t>(static_cast<float>(orig_width)*
-        ( static_cast<float>(target_size) / static_cast<float>(orig_height) ));
-    if(pad_to_square) {
-      pad_left = (target_height - target_width) >> 1; // Divide by 2
-      target_width = target_size;
-    }
-  }
-  
-  TensorInterface* scaledImg = new RamTensor({1, target_height, target_width, 3}, u8);
-  // TODO consider making these float
-  // Shift back to original scales if padding
-  const float h_scale = static_cast<float>(orig_height) / static_cast<float>(target_height - 2*pad_top); 
-  const float w_scale = static_cast<float>(orig_width) / static_cast<float>(target_width - 2*pad_left);
-  const uint16_t target_width_ubound = target_width - 2*pad_left; // ignore right padding
-  const uint16_t target_height_ubound = target_height - 2*pad_top; // ignore bottom padding
-
-  for(uint16_t out_y = 0; out_y < target_height;  out_y++) {
-    for(uint16_t out_x = 0; out_x < target_width;  out_x++) {
-      const uint16_t out_y_origin = out_y - pad_top;
-      const uint16_t out_x_origin = out_x - pad_left;
-      for(uint16_t c = 0; c < 3; c++) {
-        uint8_t out_val = 0;
-        // If in padded region
-        if (!((out_x_origin >= 0) && (out_x_origin < target_width_ubound) && (out_y_origin >= 0) &&
-            (out_y_origin < target_height_ubound))) {
-          out_val = 0;
-        } else { // We are not in padded region
-          // TODO consider doing float scaling then back to int post
-          // Note these are int on purpose so we can check bounds more easily
-          const int16_t in_y = static_cast<int16_t>(static_cast<float>(out_y_origin) * h_scale);
-          const int16_t in_x = static_cast<int16_t>(static_cast<float>(out_x_origin) * w_scale);
-          const int16_t x1 = ( (in_x - 1) > 0 ) ? in_x - 1 : 0; 
-          const int16_t y1 = ( (in_y - 1) > 0 ) ? in_y - 1 : 0; 
-          const int16_t x2 = ( (in_x + 1) < target_width_ubound  ) ? in_x + 1 : target_width_ubound-1; 
-          const int16_t y2 = ( (in_y + 1) < target_height_ubound ) ? in_y + 1 : target_height_ubound-1; 
-          
-          const uint8_t Q11 = slicedImage(y1, x1, c);
-          const uint8_t Q21 = slicedImage(y1, x2, c);
-          const uint8_t Q12 = slicedImage(y2, x1, c);
-          const uint8_t Q22 = slicedImage(y2, x2, c);
-
-          // Interpolate in x dir
-          const float fxy1 = 
-            static_cast<float>(Q11)*static_cast<float>(x2-in_x)/static_cast<float>(x2-x1) + 
-            static_cast<float>(Q21)*static_cast<float>(in_x - x1)/static_cast<float>(x2-x1);
-          const float fxy2 = 
-            static_cast<float>(Q12)*static_cast<float>(x2-in_x)/static_cast<float>(x2-x1) + 
-            static_cast<float>(Q22)*static_cast<float>(in_x - x1)/static_cast<float>(x2-x1);
-          
-          // Interpolate in y dir
-          float out_f = 
-            fxy1*static_cast<float>(y2-in_y)/static_cast<float>(y2-y1) +
-            fxy2*static_cast<float>(in_y - y1)/static_cast<float>(y2-y1);
-          out_f = (out_f > 255) ? 255 : out_f;
-          out_f = (out_f < 0) ? 0 : out_f;
-          
-          out_val = static_cast<uint8_t>(out_f);
-        } // else not in padding
-        (*scaledImg)(0, out_y, out_x, c) = out_val;
-      }
-    }
-  }
-  return scaledImg;
-}
 
 /*
  * uTensor module instantiation
@@ -147,6 +20,7 @@ localCircularArenaAllocator<2048> meta_allocator;
 localCircularArenaAllocator<4000000, uint32_t>
     ram_allocator;  // 4MB should be plenty
 SimpleErrorHandler mErrHandler(10);
+BilinearInterpolator<uint8_t> bilinear;
 
 int main(int argc, char* argv[]) {
   if (argc != 4) {
@@ -186,7 +60,7 @@ int main(int argc, char* argv[]) {
   // Keep the buffer around as we may want tu use it for something
   Tensor image_t = new BufferTensor(
       {1, height, width, 3}, u8, image_buffer);
-  SlicedImage slicedImage(image_t, num_row_slices, num_col_slices);
+  SlicedImage<uint8_t> slicedImage(image_t, num_row_slices, num_col_slices);
 
   // Output the sliced image
   bitmap_image oimage(slicedImage.sliced_width, slicedImage.sliced_height);
@@ -206,7 +80,7 @@ int main(int argc, char* argv[]) {
   // Bilinear interpolation
   slicedImage.set_current_slice(0 , 3);
   int target_size = 32;
-  Tensor scaledImage =  bilinear_interpolate(slicedImage, target_size, true);
+  Tensor scaledImage =  bilinear.interpolate(slicedImage, target_size, true);
   bitmap_image simage(target_size, target_size);
   for (int y = 0; y < scaledImage->get_shape()[1]; y++) {
     for (int x = 0; x < scaledImage->get_shape()[2]; x++) {
